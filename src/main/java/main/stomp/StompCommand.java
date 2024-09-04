f448e2c43221baff301beea8e5e48e0c7eb03d44
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-09-04 11:41:25 +0900

    add stomp command

---
+++ src/main/java/main/stomp/StompCommand.java
@@ -0,0 +1,15 @@
+package main.stomp;
+
+public enum StompCommand {
+    CONNECT,
+    CONNECTED,
+    SEND,
+    SUBSCRIBE,
+    UNSUBSCRIBE,
+    ACK,
+    NACK,
+    BEGIN,
+    COMMIT,
+    ABORT,
+    DISCONNECT
+}
